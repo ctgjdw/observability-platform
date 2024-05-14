e8c12dcf6e0ff9811f3614dd2d0910d0d6f0829d
Goh Chun Teck <gohchunteck@hotmail.com>
2024-05-14 16:09:19 +0800

    add driver-app-2 with auto-instrumentation

---
+++ driver-app-2/src/configs/generalConfig.ts
@@ -0,0 +1,3 @@
+export const API_PREPEND = "/api";
+
+export default {};
---
+++ driver-app-2/src/index.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import express, { Express } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import { API_PREPEND } from "./configs/generalConfig";
+import userRouterV1 from "./routes/v1/userRouter";
+import Logger from "./utils/logger";
+import healthCheckRouter from "./routes/healthCheckRouter";
+
+dotenv.config();
+const PORT = process.env.SERVER_PORT || 5000;
+const app: Express = express();
+
+app.use(helmet()); // secure app by setting http response headers
+app.use(express.json()); // parses incoming requests with JSON payloads
+app.use(express.urlencoded({ extended: true })); // parses urlencoded bodies with qs library
+app.use(morgan("combined")); // outputs a rich apache standard logging for every request made
+
+app.use(`${API_PREPEND}/v1/user`, userRouterV1.router);
+
+app.use("/health", healthCheckRouter.router); // add health check for container platform
+
+app.listen(PORT, async () => {
+  Logger.info(`Server is running at PORT: ${PORT}`);
+});
